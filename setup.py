bbdd9d449de3c3de5d9fe24299dba6377f3a867b
Greg Fodor <gfodor@gmail.com>
2023-03-01 14:00:46 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="madgwick_py",
+    version="1.0",
+    author="morgil",
+    author_email="none@email.com",
+    description="A Python implementation of Madgwick's IMU and AHRS algorithm",
+    long_description="A Python implementation of Madgwick's IMU and AHRS algorithm",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Curiosity-Machines/madgwick_py",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3.0",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.4',
+   
